13651b5473e9682dfe31050fa60c25e8c71ce428
bjimingtang <brendan.jiming.tang@gmail.com>
2021-04-26 21:28:29 -0700

    comments added in app.js for reference

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 //jshint esversion:6
 
+// dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
+// using mongoose to connect to mongodb
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
 mongoose.set('useFindAndModify', false);
 
+// example default content
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -20,13 +24,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// posts in the db should be stored as a set of title and content
 const postSchema = {
   title: String,
   content: String
 };
 const Post = mongoose.model("Post", postSchema);
 
+// home page
 app.get("/", function(req, res){
+  // find all posts
   Post.find({}, function(err, posts){
 
     res.render("home", {
@@ -37,25 +44,31 @@ app.get("/", function(req, res){
   });
 });
 
+// about page
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
 
+// contact page
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+// compose page
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
+// compose page submission
 app.post("/compose", function(req, res){
+  // make a new post of the submitted title and content
   const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
   });
-
+  // save the post
   post.save(function(err){
+    // if successful, redirect to home
     if (!err){
       res.redirect("/");
     }
@@ -63,9 +76,10 @@ app.post("/compose", function(req, res){
 
 });
 
+// individual post page
 app.get("/posts/:postId", function(req, res){
   const requestedId = req.params.postId;
-
+  // find the post
   Post.findOne({_id: requestedId}, function(err, post){
     res.render("post", {
       title: post.title,
@@ -74,6 +88,7 @@ app.get("/posts/:postId", function(req, res){
   });
 });
 
+// start the server
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
